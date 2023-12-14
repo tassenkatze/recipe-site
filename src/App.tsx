db2223fb8ecabecef5eb9b149780309ffdb476b8
tassenkatze <kathebner@gmail.com>
2023-12-14 02:02:22 +0100

    adding routes to webpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // components hat function, die element zurückgibt
 function App() {
 
   //return: tsx code instead of html
   return (
-    <div className="App"> {/* className instead of class in tsx or jsx code */}
-      <Header/> {/* use imported component */}
-      <Body/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App"> {/* className instead of class in tsx or jsx code */}
+        <Header /> {/* use imported component */}
+
+        <Routes>
+          <Route path="/recipe-site" element={<Body />} />
+          <Route path="/recipe-site/lego" element={<div className="Content">Lego is great</div>} />
+
+
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
