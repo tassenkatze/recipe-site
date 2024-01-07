acf214e6947b898b355f6ef058ec4a330255e6a3
tassenkatze <kathebner@gmail.com>
2024-01-07 01:32:11 +0100

    adding option to read data from local file instead of json server

--- src/customHooks/useFetch.tsx
+++ src/customHooks/useFetch.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
+import jsonData from "../data/data.json";
 
 // erstellen von custom Hook um es überall zu verwenden
 // speichern der functiion als const, Name muss mit 'use' beginnen
-const useFetch = (url: string) => {
+
+const useFetchAPI = (url: string) => {
     const [data, setData] = useState();
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
@@ -23,6 +25,7 @@ const useFetch = (url: string) => {
                 //use data from response
                 //console.log(data);
                 setData(fetchedData);
+                //console.log(data);
                 setIsPending(false);
                 setError(null);
             }))
@@ -37,4 +40,18 @@ const useFetch = (url: string) => {
     return { data, isPending, error }
 }
 
-export default useFetch;
+const useFetchJsonFile = (source: string) => {
+    const [data, setData] = useState<any[]>();
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setData(jsonData.recipies);
+        setIsPending(false);
+        setError(null);
+    })
+
+    return { data, isPending, error }
+}
+
+export default useFetchJsonFile;
